438484eb56ae50e438d190bc96f8e4509057769b
Mateusz <mateusz2003w@gmail.com>
2025-10-23 15:26:50 +0200

    error handling temporary removed

--- readers/JSON_reader.py
+++ readers/JSON_reader.py
@@ -4,13 +4,6 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 def process(data: str) -> dict|None:
-    try:
-        formatted_data = json.loads(data)
-        logging.info("JSON read")
-        return formatted_data
-    except json.JSONDecodeError as e:
-        logging.error(f"An error occurred with JSON structure: {e}")
-        return None
-    except TypeError as e:
-        logging.error(f"Type error during conversion: {e}")
-        return None
+    formatted_data = json.loads(data)
+    logging.info("JSON read")
+    return formatted_data
--- test/readers/test_YAML_reader.py
+++ test/readers/test_YAML_reader.py
@@ -52,33 +52,33 @@ def test_on_correct_yaml(caplog):
 
     assert "YAML read" in caplog.messages
 
-def test_on_incorrect_yaml_for_parser_error(caplog):
-    with caplog.at_level(logging.ERROR):
-        formatted_data = YAML_reader.process(INCORRECT_YAML_PARSER_ERROR)
-
-    assert formatted_data is None
-
-    assert ('There was an error with YAML structure: while parsing a '
-            'block mapping\n  in "<unicode string>", line 2, column '
-            '1:\n    name:\n    ^\nexpected <block end>, but found '
-            '\'<block mapping start>\'\n  in "<unicode string>", line 4, '
-            'column 5:\n        last: White\n        ^') in caplog.messages
-
-def test_on_incorrect_yaml_for_scanner_error(caplog):
-    with caplog.at_level(logging.ERROR):
-        formatted_data = YAML_reader.process(INCORRECT_YAML_SCANNER_ERROR)
-
-    assert formatted_data is None
-
-    assert ('There was an error with YAML structure: mapping values are '
-            'not allowed here\n  in "<unicode string>", line 3, column '
-            '9:\n        last: White\n            ^') in caplog.messages
-
-def test_wrong_datatype(caplog):
-    with caplog.at_level(logging.ERROR):
-        formatted_data = YAML_reader.process("qwe")
-
-    assert formatted_data is None
-
-    assert ("There was an error with given data: YAML top-level value is not"
-            " a mapping, actual type: <class 'str'>") in caplog.messages
+# def test_on_incorrect_yaml_for_parser_error(caplog):
+#     with caplog.at_level(logging.ERROR):
+#         formatted_data = YAML_reader.process(INCORRECT_YAML_PARSER_ERROR)
+#
+#     assert formatted_data is None
+#
+#     assert ('There was an error with YAML structure: while parsing a '
+#             'block mapping\n  in "<unicode string>", line 2, column '
+#             '1:\n    name:\n    ^\nexpected <block end>, but found '
+#             '\'<block mapping start>\'\n  in "<unicode string>", line 4, '
+#             'column 5:\n        last: White\n        ^') in caplog.messages
+#
+# def test_on_incorrect_yaml_for_scanner_error(caplog):
+#     with caplog.at_level(logging.ERROR):
+#         formatted_data = YAML_reader.process(INCORRECT_YAML_SCANNER_ERROR)
+#
+#     assert formatted_data is None
+#
+#     assert ('There was an error with YAML structure: mapping values are '
+#             'not allowed here\n  in "<unicode string>", line 3, column '
+#             '9:\n        last: White\n            ^') in caplog.messages
+#
+# def test_wrong_datatype(caplog):
+#     with caplog.at_level(logging.ERROR):
+#         formatted_data = YAML_reader.process("qwe")
+#
+#     assert formatted_data is None
+#
+#     assert ("There was an error with given data: YAML top-level value is not"
+#             " a mapping, actual type: <class 'str'>") in caplog.messages
