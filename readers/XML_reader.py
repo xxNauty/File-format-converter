6a732c1b61d2033bcc15b5d53a4851d1569f05c4
Mateusz <mateusz2003w@gmail.com>
2025-10-23 16:17:50 +0200

    fixed datatype problem

--- readers/XML_reader.py
+++ readers/XML_reader.py
@@ -2,9 +2,16 @@ import json
 import logging
 import xmltodict
 
+from common import datatype_fixer
+
 logging.basicConfig(level=logging.INFO)
 
 def process(data: str) -> dict|None:
     ordered = xmltodict.parse(data)
     logging.info("XML read")
-    return json.loads(json.dumps(ordered))
+    result = json.loads(json.dumps(ordered))
+    print(result)
+    for key, value in result.items():
+        result[key] = datatype_fixer.fix_datatype(value)
+
+    return result
